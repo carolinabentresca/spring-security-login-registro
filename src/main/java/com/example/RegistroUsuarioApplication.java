0e3fab633e92bd7328cb1a4381cfc2daf2b766b9
EXO <EXO@192.168.0.159>
2022-07-09 21:22:36 -0300

    Primer Commit en Proyecto Spring Boot

---
+++ src/main/java/com/example/RegistroUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroUsuarioApplication.class, args);
+	}
+
+}
